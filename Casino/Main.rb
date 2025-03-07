02d2450e5429c195d0fa673c06a9bf74e1f4c4fe
Feronarus <arhirom99@gmail.com>
2025-03-07 14:44:59 +0300

    Update Main.rb

--- Casino/Main.rb
+++ Casino/Main.rb
@@ -4,9 +4,9 @@ require './Casino.rb'
 
 gamers = Human.new
 casino = Casino.new
-if checkingForThePossibilityOfAGame?(gamers.year)
+if checking_for_the_possibility_of_a_game?(gamers.year)
 	puts "Добро поожаловать"
-	casino.startGame(gamers.wallet)
+	casino.start_game(gamers.wallet)
 else
 	puts "Вы не имеете права играть в азартные игры"
 end
